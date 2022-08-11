7fd8a49aa5c6b54537999e6cf9d30acd471a6868
Titus Mumo <titusmumo@gmail.com>
2022-08-11 22:50:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const events = []; 
+function writeCards(names) { 
+    for (let i = 0; i < names.length; i++) {
+        events.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)        
+    }
+    return events
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown > 9) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 8) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 7) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 6) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 5) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 4) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 3) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 2) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > 1) {
+        countDown --
+        console.log(countDown);
+    }
+    while (countDown > -1) {
+        countDown --
+        console.log(countDown);
+    }
+}
